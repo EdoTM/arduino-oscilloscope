f45ab386261732d4d675848a2ae325aa514168c2
edotm <edokyactus@gmail.com>
2023-06-18 18:29:31 +0200

    build: setup for arduino uno

--- src/avr_common/uart.h
+++ src/avr_common/uart.h
@@ -1,8 +1,10 @@
 #pragma once
 
 // useful defines for code completion in IDE
+#ifndef __AVR_ATmega328P__
 #define __AVR_ATmega328P__
 #define __AVR_3_BYTE_PC__
 #define F_CPU 16000000UL
+#endif
 
 void printf_init(void);
