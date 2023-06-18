e62a450f1add573529c0a0cfe26abdee636dd088
edotm <edokyactus@gmail.com>
2023-06-19 00:12:58 +0200

    feat: add analog to digital conversion

--- src/oscilloscope.c
+++ src/oscilloscope.c
@@ -2,11 +2,39 @@
 #include "avr_common/uart.h"
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+void setup_analog_to_digital_conversion() {
+    // set ADC reference voltage to AVCC
+    ADMUX |= _BV(REFS0);
+    ADMUX &= ~_BV(REFS1);
+    ADMUX &= ~_BV(ADLAR); // set ADC to 10-bit conversion
+    // set input channel to A0
+    ADMUX &= ~(_BV(MUX0) | _BV(MUX1) | _BV(MUX2) | _BV(MUX3));
+    ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0); // set ADC prescaler to 128
+
+    ADCSRA |= _BV(ADIE); // enable interrupts
+
+    ADCSRA |= _BV(ADEN); // enable ADC
+}
+
+void start_conversion() {
+    ADCSRA |= _BV(ADSC);
+}
+
+ISR(ADC_vect) {
+    uint16_t adc_value = ADC;
+    printf("%u\n", adc_value);
+}
 
 int main() {
     printf_init();
+    setup_analog_to_digital_conversion();
+
+    sei(); // enable interrupts
+
     while (1) {
-        printf("Hello, world!\n");
-        _delay_ms(1000);
+        start_conversion();
+        _delay_ms(55);
     }
 }
