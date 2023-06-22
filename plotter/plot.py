0f8324bc87e717d9d32d65c9af4a7392c97bb1af
edotm <edokyactus@gmail.com>
2023-06-23 00:18:43 +0200

    feat(plotter): use Qt widgets and add Start/Stop button

--- plotter/plot.py
+++ plotter/plot.py
@@ -1,46 +1,87 @@
-import collections
+from collections import deque
 
 import numpy as np
 import pyqtgraph as pg
 import serial
-from PyQt6 import QtWidgets
-from pyqtgraph.Qt import QtCore
+from PyQt6 import QtWidgets, QtCore
 
 
-class Plotter:
-    def __init__(self, sampleinterval=0.1, timewindow=10., size=(600, 350)):
-        # Data stuff
-        self._interval = int(sampleinterval * 1000)
-        self._bufsize = int(timewindow / sampleinterval)
-        self.buffer = collections.deque([0.0] * self._bufsize, maxlen=self._bufsize)
-        self.x = np.linspace(-timewindow, 0.0, self._bufsize)
-        self.y = np.zeros(self._bufsize, dtype=float)
+class OscilloscopePlot(pg.PlotWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.showGrid(x=True, y=True)
+        self.setLabel('left', 'Voltage', 'V')
+        self.setLabel('bottom', 'Time', 's')
+        self.curve = self.plot(pen=(255, 255, 0))
 
-        # PyQtGraph stuff
-        self.win = pg.GraphicsLayoutWidget(title="Oscilloscope", size=size, show=True)
-        self.app = QtWidgets.QApplication([])
-        self.plt = self.win.addPlot(title='Oscilloscope')
-        self.plt.showGrid(x=True, y=True)
-        self.plt.setLabel('left', 'Voltage', 'V')
-        self.plt.setLabel('bottom', 'Time', 's')
-        self.curve = self.plt.plot(self.x, self.y, pen=(255, 255, 0))
 
-        # QTimer
+class PinSelectorPanel(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.layout = QtWidgets.QHBoxLayout()
+        self.layout.addWidget(QtWidgets.QLabel('Pin:'))
+        self.pin = QtWidgets.QSpinBox()
+        self.pin.setRange(0, 5)
+        self.pin.setPrefix('A')
+        self.layout.addWidget(self.pin)
+        self.setLayout(self.layout)
+
+
+class OscilloscopeWindow(QtWidgets.QWidget):
+    def __init__(self, parent=None, interval_ms=15, timewindow=10.):
+        super().__init__(parent=parent)
+        self.setWindowTitle('Oscilloscope')
+        self.resize(600, 350)
+        self.plot = OscilloscopePlot()
+
+        self.commands = QtWidgets.QWidget()
+        commands_layout = QtWidgets.QVBoxLayout()
+        self.start_button = QtWidgets.QPushButton('Start')
+        self.start_button.clicked.connect(self.start_button_clicked)
+        commands_layout.addWidget(self.start_button)
+        self.commands.setLayout(commands_layout)
+        pins_panel = PinSelectorPanel()
+        commands_layout.addWidget(pins_panel)
+
+        self.layout = QtWidgets.QHBoxLayout()
+        self.layout.addWidget(self.plot)
+        self.layout.addWidget(self.commands)
+
+        self.setLayout(self.layout)
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.updateplot)
-        self.timer.start(self._interval)
+        self.timer.start(interval_ms)
+
+        self.timewindow = timewindow
+        self.plot.setXRange(-timewindow, 0)
+        self.bufsize = int(timewindow * 1000 / interval_ms)
+        self.buffer = deque([0.0] * self.bufsize, maxlen=self.bufsize)
+        self.x = np.linspace(-timewindow, 0.0, self.bufsize)
+        self.y = np.zeros(self.bufsize)
+        self.start = False
+
+    def start_button_clicked(self):
+        self.start = not self.start
+        if self.start:
+            self.buffer = deque([0.0] * self.bufsize, maxlen=self.bufsize)
+            self.y[:] = 0.0
+            self.start_button.setText('Stop')
+        else:
+            self.start_button.setText('Start')
 
     def updateplot(self):
-        self.buffer.append(get_voltage())
+        if not self.start:
+            return
+
+        self.buffer.append(get_voltage_from_serial())
         self.y[:] = self.buffer
-        self.curve.setData(self.x, self.y)
-        self.app.processEvents()
+        self.plot.curve.setData(self.x, self.y)
+        self.plot.setYRange(min(self.y), max(self.y))
+        self.plot.enableAutoRange('xy', False)
+        self.plot.update()
 
-    def run(self):
-        self.app.exec()
 
-
-def get_voltage():
+def get_voltage_from_serial():
     global ser
     line = ser.readline()
     try:
@@ -55,5 +96,7 @@ def get_voltage():
 ser = serial.Serial('/dev/ttyACM0', 19200, timeout=1)
 
 if __name__ == '__main__':
-    m = Plotter(sampleinterval=0.015, timewindow=10.)
-    m.run()
+    app = QtWidgets.QApplication([])
+    win = OscilloscopeWindow()
+    win.show()
+    app.exec()
