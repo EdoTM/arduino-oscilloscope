9196b1b438b7f9aabb59e1b37310e8ab2cd969a8
edotm <edokyactus@gmail.com>
2023-06-25 19:58:00 +0200

    feat: read buffer from UART

--- src/oscilloscope.c
+++ src/oscilloscope.c
@@ -2,6 +2,7 @@
 #include "avr_common/uart.h"
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <assert.h>
 #include "./adc/adc.h"
 #include "./voltage_switch/voltage_switch.h"
 #include "./square_wave/square_wave.h"
@@ -31,11 +32,20 @@ ISR(TIMER1_COMPA_vect) {
     toggle_square_wave_voltage();
 }
 
+volatile uint8_t rx_buf[8];
+volatile uint8_t rx_buf_index = 0;
+
 ISR(USART_RX_vect) {
-    uint8_t data = UDR0;
-    uint8_t pin = data - '0';
-    if (pin <= 5)
-        set_adc_pin(pin);
+    uint8_t rx_byte = UDR0;
+    if (rx_byte != '\n' && rx_byte != '\r') {
+        rx_buf[rx_buf_index++] = rx_byte;
+        return;
+    }
+    rx_buf[rx_buf_index] = '\0';
+    rx_buf_index = 0;
+    uint8_t adc_pin = strtoul((const char*) rx_buf, NULL, 10);
+    if (adc_pin >= 0 && adc_pin <= 5)
+        set_adc_pin(adc_pin);
 }
 
 int main(void) {
